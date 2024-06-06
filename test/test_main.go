95006e365d3eb61d7fdb9f5a86aa02c9558997ed
李兴华 <kmlixh@foxmail.com>
2024-06-06 10:03:04 +0800

    跟随官方升级版本

--- test/test_main.go
+++ test/test_main.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"github.com/hashicorp/consul/api"
 	"github.com/kmlixh/consulTool"
-	"io"
 	"net/http"
-	"time"
 )
 
 var config *api.Config
@@ -15,34 +13,16 @@ var serviceId = "1"
 
 func init() {
 	config = api.DefaultConfig()
-	config.Address = "10.0.1.5:8500"
+	config.Address = "192.168.110.249:8500"
 }
 func main() {
 	consulTool.Debug(false)
 	watch := consulTool.NewWatch(config)
-	watch.WatchKv("janyee/auth/prod", func(index uint64, kvPair *api.KVPair) {
+	watch.WatchKv("cors_url", func(index uint64, kvPair *api.KVPair) {
 		fmt.Printf("\nkey [%s] changed:%s\n", kvPair.Key, string(kvPair.Value))
 	})
-	agent := consulTool.NewAgent(config)
+	//agent := consulTool.NewAgent(config)
 
-	agent.Watch("mongo")
-	go func() {
-		for {
-			timeAfterTrigger := time.After(time.Millisecond * 699)
-			// will be suspend but we have `timer` so will be not deadlocked
-			<-timeAfterTrigger
-			// print current time
-			resp, er := agent.HttpClient().Get("http://mongo/hello")
-			//resp, er := agent.HttpClient().Get("https://fochan.org/admin/xxx")
-			if er != nil {
-				fmt.Println(er)
-				continue
-			}
-			bytes, err := io.ReadAll(resp.Body)
-
-			fmt.Println(err, string(bytes))
-		}
-	}()
 	// 清理计时器
 	http.ListenAndServe(":8081", nil)
 }
